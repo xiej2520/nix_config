b87913ec30e071188361d80e990f297c528936d9
xiej2520 <jackyxie2520@outlook.com>
2025-09-05 01:08:34 -0500

    working-laptop home config, improve script

--- build.sh
+++ build.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
-# Ensure HOST is set
 if [ -z "$HOST" ]; then
-  echo "HOST is not set. Please set HOST to the desired system name."
-  exit 1
+  read -p "HOST is not set. Please enter the desired system name (press Enter to use $(hostname)): " input_host
+  HOST="${input_host:-$(hostname)}"
 fi
+echo "Using HOST: '$HOST'"
 
-sudo HOST=$HOST nixos-rebuild switch --flake .#$HOST --show-trace || exit # --impure
-home-manager switch --flake .#$USER@$HOST || exit #--impure
+if [ -z "$USER" ]; then
+  read -p "USER is not set. Please enter the username (press Enter to use the $(whoami)): " input_user
+  USER="${input_user:-$(whoami)}"
+fi
+echo "Using USER: '$USER'"
 
+echo "Rebuilding and activating nixos"
+sudo HOST=$HOST nixos-rebuild switch --flake .#$HOST --show-trace # --impure
+echo "Rebuilding and activating home-manager"
+home-manager switch --flake .#$USER@$HOST #--impure

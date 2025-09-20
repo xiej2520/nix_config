d071f4a5690aeb34f729f14c213cb244309762af
xiej2520 <jackyxie2520@outlook.com>
2025-09-20 10:40:09 -0500

    organize system and home packages

--- flake.nix
+++ flake.nix
@@ -2,17 +2,14 @@
   description = "Nix Config";
 
   inputs = {
-    # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     # You can access packages and modules from different nixpkgs revs
     # at the same time. Here's an working example:
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     # Also see the 'unstable-packages' overlay at 'overlays/default.nix'.
 
-    # Home manager
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    
     #nixos-wsl.url = "github:nix-community/nixos-wsl";
   };
 
@@ -32,8 +29,6 @@
       "aarch64-darwin"
       "x86_64-darwin"
     ];
-    # This is a function that generates an attribute by calling a function you
-    # pass to it, with each system as an argument
     forAllSystems = nixpkgs.lib.genAttrs systems;
   in {
     # Your custom packages

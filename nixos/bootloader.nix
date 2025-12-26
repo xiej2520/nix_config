4772e5d1e6a8238427f88a4499b928c58d4ef1dd
xiej2520 <xiej2520@gmail.com>
2025-12-26 12:33:41 -0600

    swap partition and zswap

--- nixos/bootloader.nix
+++ nixos/bootloader.nix
@@ -18,5 +18,11 @@
     #efi.efiSysMountPoint = "/boot";
     timeout = 30;
   };
+  boot.kernelParams = [
+    "zswap.enabled=1"
+    "zswap.compressor=zstd"
+    "zswap.max_pool_percent=20"
+    "zswap.shrinker_enabled=1"
+  ];
   boot.supportedFilesystems = [ "ntfs" ];
 }

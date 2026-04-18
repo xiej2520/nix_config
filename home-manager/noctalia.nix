3b6d2555ee42ab6d4148d5709f082319a20eb969
xiej2520 <xiej2520@gmail.com>
2026-04-18 12:29:37 -0500

    config changes
    niri config
    
    symlink noctalia_settings.json

--- home-manager/noctalia.nix
+++ home-manager/noctalia.nix
@@ -5,53 +5,35 @@
     inputs.noctalia.homeModules.default
   ];
 
-  # noctalia-shell ipc call state all > dotfiles/noctalia-settings.json
+
+  # noctalia-shell ipc call state all > dotfiles/noctalia-state.json
   programs.noctalia-shell = {
     enable = true;
-    #settings = {
-    #  bar = {
-    #    density = "compact";
-    #    position = "left";
-    #    showCapsule = false;
-    #    widgets = {
-    #      left = [
-    #        {
-    #          id = "ControlCenter";
-    #          useDistroLogo = true;
-    #        }
-    #        {
-    #          id = "Network";
-    #        }
-    #        {
-    #          id = "Bluetooth";
-    #        }
-    #      ];
-    #      center = [
-    #        {
-    #          hideUnoccupied = false;
-    #          id = "Workspace";
-    #          labelMode = "none";
-    #        }
-    #      ];
-    #      right = [
-    #        {
-    #          alwaysShowPercentage = false;
-    #          id = "Battery";
-    #          warningThreshold = 30;
-    #        }
-    #        {
-    #          formatHorizontal = "HH:mm";
-    #          formatVertical = "HH mm";
-    #          id = "Clock";
-    #          useMonospacedFont = true;
-    #          usePrimaryColor = true;
-    #        }
-    #      ];
-    #    };
-    #  };
-    #  colorSchemes.predefinedScheme = "Monochrome";
-    #};
+    #settings = { };
     # this may also be a string or a path to a JSON file.
-    #settings = ./noctalia.json;
+    #settings = ./noctalia-settings.json;
+    # ~/.config/noctalia/settings.json
   };
 }
+# ~/.config/noctalia/colorschemes/custom/custom.json
+#{
+#    "dark": {
+#        "mPrimary": "#F878FF",
+#        "mOnPrimary": "#1E1E22",
+#        "mSecondary": "#A090F0",
+#        "mOnSecondary": "#1E1E22",
+#        "mTertiary": "#3CD357",
+#        "mOnTertiary": "#161616",
+#        "mError": "#EE5365",
+#        "mOnError": "#161616",
+#        "mSurface": "#161616",
+#        "mOnSurface": "#F2F4F8",
+#        "mHover": "#F85EB4",
+#        "mOnHover": "#161616",
+#        "mSurfaceVariant": "#262626",
+#        "mOnSurfaceVariant": "#DDE1E6",
+#        "mOutline": "#525252",
+#        "mShadow": "#000000"
+#    }
+#}
+#

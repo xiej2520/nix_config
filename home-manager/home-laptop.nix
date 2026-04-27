e38b47fe7ff5ba08a00cc717943273b7952c03b1
xiej2520 <xiej2520@gmail.com>
2026-04-26 21:54:07 -0500

    add udiskie

--- home-manager/home-laptop.nix
+++ home-manager/home-laptop.nix
@@ -142,6 +142,18 @@ in
 
   programs.fuzzel.enable = true;
 
+  services.udiskie = {
+    enable = true;
+    settings = {
+      # workaround for
+      # https://github.com/nix-community/home-manager/issues/632
+      program_options = {
+        # replace with your favorite file manager
+        file_manager = "${pkgs.kdePackages.dolphin}/bin/dolphin";
+      };
+    };
+  };
+
   fonts = {
     fontconfig = {
       enable = true;
